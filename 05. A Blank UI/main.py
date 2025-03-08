a72726e6edf87798ef9965bef61a3cb507239200
Aditya <adiprofitcoder@gmail.com>
2025-03-08 10:57:17 +0530

    Add files via upload

---
+++ 05. A Blank UI/main.py
@@ -0,0 +1,18 @@
+'''PyCODE | @_py.code'''
+
+# ? Importing Libraries
+from tkinter import *
+
+width = 800
+height = 500
+
+# ! Main Window
+root = Tk()
+
+root.title("My UI")
+root.geometry(
+    f"{width}x{height}"
+)
+
+# ? Run the UI
+root.mainloop()
